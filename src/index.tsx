d80b1aa8517b1ba77458cbc1bb76dc189a1b9a07
Peter Conn <conn.peter@gmail.com>
2023-08-20 19:13:38 +0000

    Re-enable ServiceWorker.

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
-// import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// const updateSW = registerSW({
-//   onNeedRefresh() {
-//     console.log("Needs refresh");
-//     updateSW();
-//   },
-//   onOfflineReady() {
-//     console.log("Offline ready");
-//   },
-// })
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log("Needs refresh");
+    updateSW();
+  },
+  onOfflineReady() {
+    console.log("Offline ready");
+  },
+})
